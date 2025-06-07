24a5459db123ed321024e3321019e84e61d56433
shujaurrahman <Shujaurrehman210@gmail.com>
2025-06-08 00:08:38 +0530

    init: saifGFx

---
+++ dbcredentials.class.php
@@ -0,0 +1,20 @@
+<?php
+class dbcredentials
+{
+    protected function hostName()
+    {
+        return "localhost";
+    }
+    protected function userName()
+    {
+        return "u899926382_saigfx";
+    }
+    protected function password()
+    {
+        return "SaiGFX@123";
+    }
+    protected function dbName()
+    {
+        return "u899926382_saigfx";
+    }
+}
